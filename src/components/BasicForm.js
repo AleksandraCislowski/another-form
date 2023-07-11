6b96189d35f3ddda4f99d1eb84568a6e2bbcb0a1
aleksandra <aleksandra.swigon91@gmail.com>
2023-07-11 19:39:50 +0200

    basic form email validation

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,6 +1,41 @@
+import useInput from "../hooks/use-input";
+
 const BasicForm = (props) => {
+  const {
+    value: enteredEmail,
+    isValid: emailIsValid,
+    hasError: emailError,
+    valueChangeHandler: emailChangeHandler,
+    inputBlurHandler: emailBlurHandler,
+    reset: emailReset,
+  } = useInput(
+    (value) =>
+      value
+        .toLowerCase()
+        .match(
+          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        ) && value.trim() !== ""
+  );
+  let formIsValid = false;
+  if (emailIsValid) {
+    formIsValid = true;
+  }
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+
+    if (!emailIsValid) {
+      return;
+    }
+    emailReset();
+  };
+
+  const emailInputClasses = emailError
+    ? "form-control invalid"
+    : "form-control";
+
   return (
-    <form>
+    <form onSubmit={formSubmissionHandler}>
       <div className='control-group'>
         <div className='form-control'>
           <label htmlFor='name'>First Name</label>
@@ -11,9 +46,18 @@ const BasicForm = (props) => {
           <input type='text' id='name' />
         </div>
       </div>
-      <div className='form-control'>
+      <div className={emailInputClasses}>
         <label htmlFor='name'>E-Mail Address</label>
-        <input type='text' id='name' />
+        <input
+          type='text'
+          id='name'
+          value={enteredEmail}
+          onChange={emailChangeHandler}
+          onBlur={emailBlurHandler}
+        />
+        {emailError && (
+          <p className='error-text'>Please enter a valid email.</p>
+        )}
       </div>
       <div className='form-actions'>
         <button>Submit</button>
